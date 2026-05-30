c0406336261cf859814878fc3221cfc7dc06a536
Daniya Khamzinova <daniyakhamzinova07@gmail.com>
2026-05-30 17:55:23 +0500

    update project

---
+++ public/firebase-config.js
@@ -0,0 +1,11 @@
+// Firebase configuration (externalized so it's only declared once)
+// If you rotate keys, update this file and commit.
+window.FIREBASE_CONFIG = {
+  apiKey: 'AIzaSyCmcfcLqvVCG5rWbzvFf7Oblc8ipmBlVWQ',
+  authDomain: 'aetherprotocol-310c0.firebaseapp.com',
+  projectId: 'aetherprotocol-310c0',
+  storageBucket: 'aetherprotocol-310c0.firebasestorage.app',
+  messagingSenderId: '635629853210',
+  appId: '1:635629853210:web:be471118edd7b56f884fa3',
+  measurementId: 'G-0JBF2FJT7K'
+};
